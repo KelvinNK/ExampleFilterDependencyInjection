66eedee4f635377b76ef7f1f610dde2768f3a569
Kelvin Nobre Krull <Kelvinnkrull@hotmail.com>
2021-03-15 21:51:13 -0300

    Full example code

---
+++ ExampleFilterDependencyInjection/Util/CustomFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleFilterDependencyInjection.Util
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class CustomFilter : Attribute, IAuthorizationFilter, IFilterFactory
+    {
+        private IConfiguration _configuration;
+
+        public bool IsReusable => throw new NotImplementedException();
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return new CustomFilter((IConfiguration)serviceProvider.GetService(typeof(IConfiguration)));
+        }
+
+        public CustomFilter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var requestKey = context.HttpContext.Request.Headers["Key"].ToString();
+            var secretKey = _configuration["Key"].ToString();
+
+            if(requestKey != secretKey)
+                context.Result = new UnauthorizedResult();
+        }
+    }
+}
